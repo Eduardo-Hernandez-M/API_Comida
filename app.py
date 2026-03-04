65a2e90d01383d23017e9f2f9fea4de615350854
Eduardo <l22590417@sjuanrio.tecnm.mx>
2026-03-03 18:43:26 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv 
+
+#Cargar las variables de entorno
+load_dotenv()
+
+#crear instancia
+app =  Flask(__name__)
+
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
