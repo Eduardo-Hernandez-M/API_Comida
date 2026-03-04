50a918553e6775b758da26417be33aca7b5f3a94
Eduardo <l22590417@sjuanrio.tecnm.mx>
2026-03-03 19:05:14 -0600

    Add comidas, eliminar y actualizar

--- app.py
+++ app.py
@@ -36,5 +36,54 @@ def get_comidas():
         })
     return jsonify(lista_comidas)
 
+#endpoint para obtener una comida por el id_comida
+@app.route('/comidas/<id_comida>', methods=['GET'])
+def get_comida(id_comida):
+    comida = Comidas.query.get(id_comida)
+    if comida is None:
+        return jsonify({'msj': 'Comida no encontrada'})
+    return jsonify({
+        'id_comida': comida.id_comida,
+        'nombre': comida.nombre,
+        'precio': comida.precio,
+    })
+
+#endpoint para eliminar una comida por el id_comida
+@app.route('/comidas/<id_comida>', methods=['DELETE'])
+def delete_comida(id_comida):
+    comida = Comidas.query.get(id_comida)
+    if comida is None:
+        return jsonify({'msj': 'Comida no encontrada'})
+    db.session.delete(comida)
+    db.session.commit()
+    return jsonify({'msj': 'Comida eliminada correctamente'})
+
+#endpoint para agregar una nueva comida
+@app.route('/comidas', methods=['POST'])
+def insert_comida():
+    data = request.get_json()
+    nueva_comida = Comidas(
+        id_comida=data['id_comida'],
+        nombre=data['nombre'],
+        precio=data['precio'],
+    )
+    db.session.add(nueva_comida)
+    db.session.commit()
+    return jsonify({'msg': 'Comida agregada correctamente'})
+
+#endpoint para actualizar una comida por el id_comida
+@app.route('/comidas/<id_comida>', methods=['PUT'])
+def update_comida(id_comida):
+    comida = Comidas.query.get(id_comida)
+    if comida is None:
+        return jsonify({'msj': 'Comida no encontrada'})
+    data = request.get_json()
+    if 'nombre' in data:
+        comida.nombre = data['nombre']
+    if 'precio' in data:
+        comida.precio = data['precio']
+    db.session.commit()
+    return jsonify({'msj': 'Comida actualizada correctamente'})
+
 if __name__ == '__main__':
     app.run(debug=True)
