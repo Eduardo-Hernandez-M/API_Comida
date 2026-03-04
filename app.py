09067b6609e9aa07c6d13bfdc8f9e8ee7922ffb8
Eduardo <l22590417@sjuanrio.tecnm.mx>
2026-03-03 18:54:18 -0600

    Add Comidas model and endpoint to retrieve all meals

--- app.py
+++ app.py
@@ -16,5 +16,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+#Modelo de la base de datos
+class Comidas(db.Model):
+    __tablename__ = 'comidas'
+    id_comida = db.Column(db.String, primary_key=True)
+    nombre = db.Column(db.String)
+    precio = db.Column(db.String)
+
+#endpoint para obtener todas las comidas
+@app.route('/comidas', methods=['GET'])
+def get_comidas():
+    comidas = Comidas.query.all()
+    lista_comidas = []
+    for comida in comidas:
+        lista_comidas.append({
+            'id_comida': comida.id_comida,
+            'nombre': comida.nombre,
+            'precio': comida.precio,
+        })
+    return jsonify(lista_comidas)
+
 if __name__ == '__main__':
     app.run(debug=True)
